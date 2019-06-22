c93d978c5e13f1384528648391380823e572a9f2
Gabriel Horner <gabriel.horner@gmail.com>
2019-06-22 09:19:19 -0400

    Add 2 joke github commands for push and pull

--- src/git_puns/commands.cljs
+++ src/git_puns/commands.cljs
@@ -1,6 +1,8 @@
 (ns git-puns.commands
-  (require [clojure.string :as string]))
+  (:require [clojure.string :as string]))
 
+;; Joke command
+;; ============
 (def fs (js/require "fs"))
 ;; From https://github.com/EugeneKay/git-jokes/
 (def jokes-file (str js/__dirname "/../resources/jokes.txt"))
@@ -12,4 +14,21 @@
 (defn- joke-command []
   (.. js/atom -notifications (addInfo (fetch-joke) #js{:dismissable true})))
 
-(def commands {:joke joke-command})
+;; Wrapped github commands
+;; =======================
+(defn- invoke-commands [commands]
+  (let [editor (js/atom.workspace.getActiveTextEditor)
+        editor-view (js/atom.views.getView editor)]
+    (doseq [cmd commands]
+      (js/atom.commands.dispatch editor-view cmd))))
+
+(def github-push-with-joke-command
+  (partial invoke-commands ["github:push" "git-puns:joke"]))
+
+(def github-pull-with-joke-command
+  (partial invoke-commands ["github:pull" "git-puns:joke"]))
+
+;; Exported commands
+(def commands {:joke joke-command
+               :github-push-with-joke github-push-with-joke-command
+               :github-pull-with-joke github-pull-with-joke-command})
